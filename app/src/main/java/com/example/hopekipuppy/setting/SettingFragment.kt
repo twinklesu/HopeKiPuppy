3141e3607e389ddb689f51c0b302eafea695baf3
Subin Park <twinklesu914@gmail.com>
2020-11-20 23:12:28 +0900

    setting package 생성

--- app/src/main/java/com/example/hopekipuppy/setting/SettingFragment.kt
+++ app/src/main/java/com/example/hopekipuppy/setting/SettingFragment.kt
@@ -1,4 +1,4 @@
-package com.example.hopekipuppy
+package com.example.hopekipuppy.setting
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -6,8 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import com.example.hopekipuppy.R
 import com.example.hopekipuppy.databinding.FragmentSettingBinding
-import com.example.hopekipuppy.databinding.FragmentWriteFoundBinding
 
 
 class SettingFragment : Fragment() {
